d1c3410f87c16c95f0a0ef84f35f3f59e4e745b3
AounAli2k06 <aounali2k06@gmail.com>
2023-06-18 10:17:25 -0700

    hello

--- frontend/src/components/login/login.tsx
+++ frontend/src/components/login/login.tsx
@@ -1,57 +1,69 @@
 import { useState } from "react";
-import login_image from "../../assets/20945597.jpg"
-import logo from "../../assets/images.png"
+import login_image from "../../assets/left side image1.jpg"
 import style from './login.module.css'
-import alert_image from '../../assets/alert_image.svg'
+import LoginModal from "../../interfaces/login";
+import axios from "axios";
+// import 'antd/dist/antd.css';
+// import { Button } from 'antd';
 
-
-function Login()
-{
+function Login() {
     const [email, setEmail] = useState<string>("");
     const [password, setPassword] = useState<string>("");
-    const [alertImageHidden, setHidden] = useState<boolean>(true);
+    const [alertBorderColor, setalertBorderColor] = useState<string>("");
 
-    function Email_OnChangeHandler(value : React.ChangeEvent<HTMLInputElement>)
-    {
+    function Email_OnChangeHandler(value: React.ChangeEvent<HTMLInputElement>) {
         setEmail(value.target.value);
     }
-    function Pass_OnChange(pass : React.ChangeEvent<HTMLInputElement>) {
-        setPassword(pass.target.value);   
-    }
-    
-    function ValidateEmail(emailValue: string) : boolean
-    {
-        const emailRegex: RegExp = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        return emailRegex.test(emailValue);
-    }
-    
-    function LoginButton_OnClickHandler() {
-        const returnedValue:boolean = ValidateEmail(email);
-        setHidden(returnedValue);
+
+    function Pass_OnChange(pass: React.ChangeEvent<HTMLInputElement>) {
+        setPassword(pass.target.value);
     }
 
-    return(
-        <div className={style["main"]}>
-             <div className={style["navbar"]}>
-            <img src={logo} alt="" className={style["logo"]} />
-                <img src={login_image} className={style["login_image"]} alt="" />
+    function handleKeyPress(event: React.KeyboardEvent<HTMLInputElement>) {
+        if (event.key==="Enter") {
+            LoginButton_OnClickHandler();
+        }
+    }
+
+    function LoginButton_OnClickHandler() {
+        const body: LoginModal = {
+            Name: email,
+            Password: password
+        };
+
+        axios.post("http://192.168.0.33:3000/login", body)
+        .then((response) => {
+            console.log("Response from server : ", response);
+        })
+        .catch((error) => {
+            console.log("Error from API : ", error);
+        });
+    }
+
+    const backgroundImageStyle = {
+        backgroundImage: `url(${login_image})`
+    };
+
+    return (
+        <div className={style["flex-container"]}>
+            <div className={style["left"]} style={backgroundImageStyle}>
+                <div className={style["heading"]}>
+                    <h1> The Password <br /> Keeper</h1>
+                </div>
             </div>
-            <div className={style["login_form"]}>
-                <div className={style["content"]}>
-                    <h1 className={style["heading"]}><span>The</span> Password <br /> Keeper</h1>
-                    <h1 className={style["login"]}>LOGIN</h1>
+            <div className={style["content"]}>
+                <div className={style["right"]}>
+                    <h1 className="main-heading">Login</h1>
                     <div className={style["seperator"]}></div>
-                    <p className={style["ptag1"]}>Email :</p>
-                    <input value={email} onChange={Email_OnChangeHandler} type="email" name="" className={style["user"]}  placeholder="Enter Email" />
-                    <img  hidden={alertImageHidden} src={alert_image} className={style["email_alert_image"]} alt="" />
-                    <p className={style["ptag2"]}>Password :</p>
-                    <input value={password} type="password" className={style["password"]} onChange={Pass_OnChange} placeholder="Enter Password" />
-                    <button onClick={LoginButton_OnClickHandler}>Login</button>
-                    <p className={style["bottom"]}>don't have an account? <a href="">signup</a></p>
+                    <p>Email: <input type="text" value={email} placeholder="Enter Email"
+                        onChange={Email_OnChangeHandler}
+                        style={{ borderColor: alertBorderColor }} id="border-element"
+                        className={`${style["shake-animation"]} ${alertBorderColor === "red" ? style["shake-animation"] : " "}`} /></p>
+                    <p>Password: <input type="password" value={password} placeholder="Enter Password" onChange={Pass_OnChange} onKeyPress={handleKeyPress} /></p>
+                    <button onClick={LoginButton_OnClickHandler} className={style["login-button"]}>Login</button>
                 </div>
             </div>
         </div>
-
     )
 }
 
