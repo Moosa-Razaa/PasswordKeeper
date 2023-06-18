a5e91866ca2d9e52dfd2914fdfdfce60d5b3576c
Moosa-Razaa <moosaraza1k99@gmail.com>
2023-06-18 22:15:49 +0500

    Sample backend API

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,10 +3,25 @@ import * as http from "http";
 const hostName: string = "localhost";
 const port: number = 3000;
 
+interface LoginModel
+{
+    Name: string,
+    Password: string
+}
+
 const server: http.Server = http.createServer((req: http.IncomingMessage, res: http.ServerResponse) => {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/plain");
-    res.end("Hello World!");
+
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    
+    if(req.method === "POST" && req.url === "/login")
+    {
+        console.log("Request hit");
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        res.end("User successfully created.");
+    }
 });
 
 server.listen(port, hostName, () => {
