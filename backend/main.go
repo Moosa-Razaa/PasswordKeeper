981f8c8ce9db386bae007f4f9c3dba558cf4d35e
Moosa-Razaa <moosaraza1k99@gmail.com>
2024-09-13 00:29:25 +0200

    feat<backend>: initialized a simple hello world api

--- backend/main.go
+++ backend/main.go
@@ -1,25 +1,24 @@
 package main
 
 import (
-  "fmt"
+	"fmt"
+	"log"
+	"net/http"
 )
 
-//TIP To run your code, right-click the code and select <b>Run</b>. Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.
-
-func main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined or highlighted text
-  // to see how GoLand suggests fixing it.
-  s := "gopher"
-  fmt.Println("Hello and welcome, %s!", s)
-
-  for i := 1; i <= 5; i++ {
-	//TIP You can try debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-	// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>. To start your debugging session, 
-	// right-click your code in the editor and select the <b>Debug</b> option. 
-	fmt.Println("i =", 100/i)
-  }
+func HelloWorld(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Received request from %s %s", r.RemoteAddr, r.URL.Path)
+	_, err := fmt.Fprintf(w, "Hello, World!")
+	if err != nil {
+		log.Fatalf("Error: %v", err)
+	}
 }
 
-//TIP See GoLand help at <a href="https://www.jetbrains.com/help/go/">jetbrains.com/help/go/</a>.
-// Also, you can try interactive lessons for GoLand by selecting 'Help | Learn IDE Features' from the main menu.
+func main() {
+	http.HandleFunc("/", HelloWorld)
+	log.Println("Server started on port 8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatalf("Error: %v", err)
+	}
+}
