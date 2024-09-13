771bfe66dd6d3775a877331867cf6bcf62a0233f
Moosa-Razaa <moosaraza1k99@gmail.com>
2024-09-13 02:31:19 +0200

    feat<backend>: initialized a new backend because previous backend started to give module errors

--- backend/main.go
+++ backend/main.go
@@ -1,24 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"net/http"
-)
-
-func HelloWorld(w http.ResponseWriter, r *http.Request) {
-	log.Printf("Received request from %s %s", r.RemoteAddr, r.URL.Path)
-	_, err := fmt.Fprintf(w, "Hello, World!")
-	if err != nil {
-		log.Fatalf("Error: %v", err)
-	}
-}
+import "fmt"
 
 func main() {
-	http.HandleFunc("/", HelloWorld)
-	log.Println("Server started on port 8080")
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		log.Fatalf("Error: %v", err)
-	}
+	fmt.Println("Hello, World!")
 }
