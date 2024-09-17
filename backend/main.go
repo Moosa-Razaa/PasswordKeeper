3300fc096377df5ed9641fa45bcdfe9afcf40da4
Moosa-Razaa <moosaraza1k99@gmail.com>
2024-09-18 00:14:38 +0200

    feat<Backend>: initialized file handler in the main file

--- backend/main.go
+++ backend/main.go
@@ -3,10 +3,25 @@ package main
 import (
 	"backend/controllers"
 	"backend/middlewares"
+	"backend/repositories"
+	"log"
 	"net/http"
 )
 
+var fileHandler *repositories.FileHandler
+
+func InitializeFileHandler() {
+	var fileHandlerError error
+
+	fileHandler, fileHandlerError = repositories.GetFileHandlerInstance()
+	if fileHandlerError != nil {
+		log.Fatalf("Error while initializing file handler: %v", fileHandlerError)
+	}
+}
+
 func main() {
+	InitializeFileHandler()
+
 	http.Handle("/generate/password", middlewares.VerifyPost(http.HandlerFunc(controllers.PasswordGeneratorController)))
 	_ = http.ListenAndServe(":8080", nil)
 }
