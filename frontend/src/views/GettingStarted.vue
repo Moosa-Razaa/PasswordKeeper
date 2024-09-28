60700ebdc00d918c8d5dfec49ecc22b066647c88
Moosa-Razaa <moosaraza1k99@gmail.com>
2024-09-28 09:21:51 +0200

    remove<tailwind>

--- frontend/src/views/GettingStarted.vue
+++ frontend/src/views/GettingStarted.vue
@@ -9,7 +9,7 @@ export default defineComponent({
 
 <template>
     <div class="mainDiv">
-        <h2 class="">Password Keeper</h2>
+        <h2 class="bg-red">Password Keeper</h2>
         <div>
             <h3>Welcome to Password Keeper</h3>
             <p>Enter your password to get started</p>
